08dc5a6f642813f496d9512f4499760fa509c063
sreedhar <sreedhar70755@gmail.com>
2023-08-25 19:41:50 +0530

    tf

--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,4 @@
 
 yum install ansible -y $>>/opt/userdata.log
 
-ansible-pull -i localhost, -U https://github.com/sreedharm07/learn-ansible.git main.yml -e component=${component}
+ansible-pull -i localhost, -U https://github.com/sreedharm07/learn-ansible.git main.yml -e component=${component}  $>>/opt/userdata.log
