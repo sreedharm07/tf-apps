7c6f7fc93ec72071ce4418561b8a2f1af6fdcee3
sreedhar <sreedhar70755@gmail.com>
2023-08-29 05:29:12 +0530

    tf

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
-python3.11-pip.noarch install boto3 botocore -y &>>/opt/userdata.log
+sudo pip3.11 install boto3 botocore -y &>>/opt/userdata.log
 
 ansible-pull -i localhost, -U https://github.com/sreedharm07/learn-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
 
