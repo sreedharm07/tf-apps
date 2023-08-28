3fdc0970f2f68979a1e3246c2dc1d8eb405fc757
sreedhar <sreedhar70755@gmail.com>
2023-08-28 16:07:51 +0530

    tf

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ resource "aws_lb_target_group" "public" {
 resource "aws_lb_target_group_attachment" "public" {
   count = data.dns_a_record_set.public.addrs
   target_group_arn = aws_lb_target_group.public[0].arn
-  target_id        = data.dns_a_record_set.public.addrs
+  target_id        = element(data.dns_a_record_set.public.addrs,count.index)
   port             = 80
 }
 
