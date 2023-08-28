5fc25dec0fb829a5acad60bf2e6aaac7ce37e64a
sreedhar <sreedhar70755@gmail.com>
2023-08-28 19:59:49 +0530

    tf

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_launch_template" "main" {
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
       component= var.components
+      env=var.env
     }))
   tag_specifications {
     resource_type = "instance"
--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum install ansible -y &>>/opt/userdata.log
+yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
 
-ansible-pull -i localhost, -U https://github.com/sreedharm07/learn-ansible.git main.yml -e component=${component}  &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/sreedharm07/learn-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
 
 
