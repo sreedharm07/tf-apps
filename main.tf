c17130852a0f88d1998b4c317cdcbf6e84e6fbda
sreedhar <sreedhar70755@gmail.com>
2023-08-28 16:21:59 +0530

    tf

--- main.tf
+++ main.tf
@@ -96,6 +96,7 @@ resource "aws_lb_listener_rule" "main" {
 resource "aws_lb_target_group" "public" {
   count = var.components == "frontend" ? 1:0
   name     = "${local.names}-public"
+  target_type = "ip"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.default_vpc
@@ -106,6 +107,7 @@ resource "aws_lb_target_group_attachment" "public" {
   target_group_arn = aws_lb_target_group.public[0].arn
   target_id        = element(tolist(data.dns_a_record_set.public.addrs),count.index)
   port             = 80
+  availability_zone = "all"
 }
 
 #resource "aws_lb_listener_rule" "main" {
