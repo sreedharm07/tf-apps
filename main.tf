341a1ed1f55a435a5534dc1bd7c979ce8b3a5e14
sreedhar <sreedhar70755@gmail.com>
2023-09-11 13:01:04 +0530

    tf

--- main.tf
+++ main.tf
@@ -119,22 +119,23 @@ resource "aws_lb_listener_rule" "main" {
 }
 
 resource "aws_lb_target_group" "public" {
-  count = var.components == "frontend" ? 1:0
-  name     = "${local.names}-public"
+  count       = var.components == "frontend" ? 1 : 0
+  name        = "${local.names}-public"
   target_type = "ip"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.default_vpc
-  tags = merge(local.tags, { Name = "${local.names}-tg" })
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.default_vpc
+  tags        = merge(local.tags, { Name = "${local.names}-tg" })
   health_check {
-    enabled = true
-    healthy_threshold = 2
-    interval = 5
-    matcher = "404"
-    path = "/"
-    port = "80"
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    matcher             = "404"
+    path                = "/"
+    port                = "80"
     unhealthy_threshold = 3
   }
+}
 
 resource "aws_lb_target_group_attachment" "public" {
   count             = var.components == "frontend" ? length(tolist(data.dns_a_record_set.public.addrs)) : 0
