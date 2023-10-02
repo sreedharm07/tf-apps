78b4df6dff1d47432d11281c427b91a8222e5947
sreedhar <sreedhar70755@gmail.com>
2023-10-02 12:39:49 +0530

    tf

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
-sudo pip3.11 install boto3 botocore &>>/opt/userdata.log
-
 wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem &>>/opt/userdata.log
 
 ansible-pull -i localhost, -U https://github.com/sreedharm07/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
