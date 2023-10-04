e64e493845cbb01b1adeb12cec296724b56edfe2
sreedhar <sreedhar70755@gmail.com>
2023-10-04 21:38:18 +0530

    tf

--- main.tf
+++ main.tf
@@ -114,6 +114,7 @@ resource "aws_lb_target_group" "main" {
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  deregistration_delay = 10
   health_check {
     enabled             = true
     healthy_threshold   = 2
